94ba676d9dc34ad1e2610d78a3f95f8662aabc30
Tian Qiu <tian.qiu142@gmail.com>
2020-09-29 20:23:20 -0700

    updates

--- poem.py
+++ poem.py
@@ -1,1 +1,18 @@
-print('supbrh')
+
+
+poem = open("poem.txt")
+
+
+def get_file_lines(filename):
+    lines_list = filename.readlines()
+    print(lines_list)
+
+
+def lines_printed_backwards(filename):
+    lines_list = filename.readlines()
+    for line in reversed(lines_list):
+        print(line)
+
+
+get_file_lines(poem)
+lines_printed_backwards(poem)
