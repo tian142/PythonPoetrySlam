6ddaa7ca8c5df2dac7d637ff7fa1265bb2a3b68c
Tian Qiu <tian.qiu142@gmail.com>
2020-09-29 20:49:33 -0700

    completed project

--- poem2.py
+++ poem2.py
@@ -19,6 +19,20 @@ def lines_printed_random(lines_list):
         print(line)
 
 
+def lines_printed_custom(lines_list):
+    print('------------------------Line of the day-------------------------')
+    print(f'Your line of the day is: {random.choice(lines_list)}')
+    ask = input(
+        'Do you like your line? you get one chance to change it. (y/n) ')
+    if ask == "n":
+        print(f'Your line of the day is: {random.choice(lines_list)}')
+    else:
+        return
+
+
 get_file_lines(lines_list)
 lines_printed_backwards(lines_list)
 lines_printed_random(lines_list)
+lines_printed_custom(lines_list)
+
+poem.close()
